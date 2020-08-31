084e687481d5c02f9d63a8b3b507d7c5a8848aa2
vickyfto <vickyfitrio19@gmail.com>
2020-09-01 00:26:31 +0700

    Updates

---
+++ precache-manifest.5c131160e885507dc6051de712d4694b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd98f349433dc52f12f5215b8c676e64",
+    "url": "/react-simple-todo/index.html"
+  },
+  {
+    "revision": "37c6fe5ed5d6c4d5dbad",
+    "url": "/react-simple-todo/static/css/main.5b83611c.chunk.css"
+  },
+  {
+    "revision": "f90261cfd89d614b5b33",
+    "url": "/react-simple-todo/static/js/2.2daf5f13.chunk.js"
+  },
+  {
+    "revision": "37c6fe5ed5d6c4d5dbad",
+    "url": "/react-simple-todo/static/js/main.054c8f72.chunk.js"
+  },
+  {
+    "revision": "41bdafc77f6d21584601",
+    "url": "/react-simple-todo/static/js/runtime-main.a04d64e6.js"
+  }
+]);
